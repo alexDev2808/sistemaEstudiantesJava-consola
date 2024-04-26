8da5be90c109c22341ed767f7c30f5387e713c6c
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-25 20:22:15 -0600

    Creando Conexion con mysql

---
+++ src/main/java/jath/conexion/Conexion.java
@@ -0,0 +1,36 @@
+package jath.conexion;
+import jath.env.Variables;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    public static Connection getConexion() {
+        Connection conexion = null;
+        String nombreDB = "estudiantes_db";
+        String url = "jdbc:mysql://localhost:3306/" + nombreDB;
+        String usuario = Variables.username;
+        String password = Variables.password;
+
+        //Cargamos la clase del driver de mysql en memoria
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, usuario, password);
+        } catch (ClassNotFoundException | SQLException e ) {
+            System.out.println("Ocurrio un error en la conexion: " + e.getMessage());
+        }
+
+
+        return conexion;
+    }
+
+    public static void main(String[] args) {
+        var conn = Conexion.getConexion();
+        if (conn != null){
+            System.out.println("Conexion exitosa! :) => " + conn);
+        } else {
+            System.out.println("Error al conectarse :(");
+        }
+    }
+}
