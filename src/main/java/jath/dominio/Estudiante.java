813de99f7db144540ca6bf928efea2a3da74f6f0
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-25 20:38:25 -0600

    Clase Estudiante

---
+++ src/main/java/jath/dominio/Estudiante.java
@@ -0,0 +1,82 @@
+package jath.dominio;
+
+public class Estudiante {
+    private int idEstudiante;
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+    private String email;
+
+    public Estudiante() {}
+
+    public Estudiante( int idEstudiante ) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    public Estudiante(String nombre, String apellidos, String telefono, String email) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public Estudiante(int idEstudiante, String nombre, String apellidos, String telefono, String email) {
+        this.idEstudiante = idEstudiante;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+
+    public int getIdEstudiante() {
+        return idEstudiante;
+    }
+
+    public void setIdEstudiante(int idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "idEstudiante=" + idEstudiante +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
